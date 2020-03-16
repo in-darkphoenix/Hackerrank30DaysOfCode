99794269fbd2cee7c2821ce944097ba497868bfb
Ankit <ph.official.ankit@gmail.com>
2020-03-16 10:59:08 +0530

    Day 3

--- Hackerrank30Days/src/day1to10/Conditional$3.java
+++ Hackerrank30Days/src/day1to10/Conditional$3.java
@@ -1,5 +1,25 @@
 package day1to10;
 
+import java.util.Scanner;
+
 public class Conditional$3 {
+	public static void main(String[] args) {
+		Scanner scn=new Scanner(System.in);
+		int n=scn.nextInt();
+		if(n%2==0) {
+			if(n>=2&&n<=5) {
+				System.out.println("Not Weird");
+			}
+			else if(n>=6&&n<=20) {
+				System.out.println("Weird");
+			}
+			else {
+				System.out.println("Not Weird");
+			}
+		}
+		else {
+			System.out.println("Weird");
+		}
+	}
 
 }
